757816c6fdfbca24ea7422920372ad3ebda56fa1
Ogar <kamil.sobczyk@wp.pl>
2019-05-07 10:50:05 +0200

    implement app bar

--- client/src/components/nav.tsx
+++ client/src/components/nav.tsx
@@ -4,52 +4,22 @@ import * as styled from "styled-components";
 
 import { Button } from "@rmwc/button";
 
-
 import { SimpleTopAppBar } from "@rmwc/top-app-bar";
 import { TopAppBarFixedAdjust } from "@rmwc/top-app-bar";
 
-
-
-const styles = {
-  root: {
-    flexGrow: 1
-  },
-  grow: {
-    flexGrow: 1
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20
-  }
-};
-
 export class Navbar extends React.Component<{}, {}> {
-    /*onClick = ():void => {
-        console.log("btn pushed");
-    }*/
-    render(){
-    return (<>
-    {/* <Button></Button> */}
-    <p>Navbar</p>
-  {/* <SimpleTopAppBar
-    title="test"
-    navigationIcon={{ onClick: () => console.log('Navigate') }}
-    actionItems={[
-      {
-        icon: 'file_download',
-        onClick: () => console.log('Do Something')
-      },
-      { icon: 'print', onClick: () => console.log('Do Something') },
-      { icon: 'bookmark', onClick: () => console.log('Do Something') }
-    ]}
-  />
-  <TopAppBarFixedAdjust />
-
-  <div style={{ height: '100rem', padding: '1rem' }}>Scroll Me</div> */}
-</>)
-    }
-
-} 
-
-//   const NabarContainer = styled.div`
-//   flexGrow: 1`
+  render() {
+    return (
+      <>
+        <SimpleTopAppBar
+          title="App Bar"
+          navigationIcon={{ onClick: () => console.log("Navigate") }}
+          actionItems={[
+            { icon: "bookmark", onClick: () => console.log("Do Something") }
+          ]}
+        />
+        <TopAppBarFixedAdjust />
+      </>
+    );
+  }
+}
