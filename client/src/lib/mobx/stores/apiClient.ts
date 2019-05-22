c9ede81527446441c147d86d36ec0943f9690986
Ogar <kamil.sobczyk@wp.pl>
2019-05-22 09:39:23 +0200

    add headers, to store and http methods

--- client/src/lib/mobx/stores/apiClient.ts
+++ client/src/lib/mobx/stores/apiClient.ts
@@ -3,10 +3,15 @@ import axios, { AxiosResponse } from 'axios';
 import { Store } from '../rootStore';
 
 import { Item, ListType, Cost } from '../../interfaces';
+import { observable } from 'mobx';
 
 const localhost = 'http://localhost:8080/';
-const herokuApp = 'https://shopping-1111.herokuapp.com/'
+const herokuApp = 'https://shopping-1111.herokuapp.com/';
 const server = localhost;
+
+interface Headers {
+  token: string;
+}
 
 export class ApiClient {
   store: Store;
@@ -14,24 +19,39 @@ export class ApiClient {
     this.store = store;
   }
 
+  @observable headers: Headers = {
+    token: 'test token'
+  };
+
+  setUserToken = (token: string): string => (this.headers.token = token);
+
   getItems = async (): Promise<Item[]> =>
-    await axios
-      .get<Item[]>(server + 'store/items')
-      .then(items => (this.store.items = items.data as Item[]));
+    await axios({
+      method: 'get',
+      url: server + 'store/items',
+      headers: this.headers
+    }).then(items => (this.store.items = items.data as Item[]));
 
   getSelected = async (): Promise<Item[]> =>
-    await axios
-      .get<Item[]>(server + 'store/selected')
-      .then(selected => (this.store.selected = selected.data as Item[]));
+    await axios({
+      method: 'get',
+      url: server + 'store/selected',
+      headers: this.headers
+    }).then(selected => (this.store.selected = selected.data as Item[]));
 
   getCosts = async (): Promise<Cost[]> =>
-    await axios
-      .get<Cost[]>(server + 'store/costs')
-      .then(costs => (this.store.costs = costs.data as Cost[]));
+    await axios({
+      method: 'get',
+      url: server + 'store/costs',
+      headers: this.headers
+    }).then(costs => (this.store.costs = costs.data as Cost[]));
 
   deleteItemOnServer = async (index: number): Promise<void> => {
-    await axios.delete(server + 'store/items', {
-      data: { index: index, user: this.store.userEmail }
+    await axios({
+      method: 'delete',
+      url: server + 'store/items',
+      headers: this.headers,
+      data: { index }
     });
   };
 
@@ -40,8 +60,11 @@ export class ApiClient {
     index: number,
     newItem: Item
   ): Promise<void> => {
-    await axios.put<Item>(server + 'store/' + list, {
-      data: { index, newItem, user: this.store.userEmail }
+    await axios({
+      method: 'put',
+      url: server + 'store/' + list,
+      headers: this.headers,
+      data: { index, newItem }
     });
   };
 
@@ -49,27 +72,38 @@ export class ApiClient {
     items: Item[],
     selected: Item[]
   ): Promise<void> => {
-    await axios.put<Item[]>(server + 'store/', {
-      data: { items, selected, user: this.store.userEmail }
+    await axios({
+      method: 'put',
+      url: server + 'store/',
+      headers: this.headers,
+      data: { items, selected }
     });
   };
 
   addCostOnServer = async (cost: Cost): Promise<void> => {
-    await axios.post<Cost>(server + 'store/costs', {
-      data: { cost, user: this.store.userEmail }
+    await axios({
+      method: 'post',
+      url: server + 'store/costs',
+      headers: this.headers,
+      data: { cost }
     });
   };
 
   checkItemOnServer = async (list: ListType, index: number): Promise<void> => {
-    await axios.put<Item>(server + 'store/checked', {
-      data: { list, index, user: this.store.userEmail }
+    await axios({
+      method: 'put',
+      url: server + 'store/checked',
+      headers: this.headers,
+      data: { list, index }
     });
   };
 
   addItemOnServer = async (item: Item): Promise<void> => {
-    await axios.post<Item>(server + 'store/items', {
-      data: { item },
-      user: this.store.userEmail
+    await axios({
+      method: 'post',
+      url: server + 'store/items',
+      headers: this.headers,
+      data: { item }
     });
   };
 }
