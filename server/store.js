d23b5a3fded2e2ec6339ee97397d94a514d3ce86
Ogar <kamil.sobczyk@wp.pl>
2019-05-22 14:47:14 +0200

    move store to different mofule

---
+++ server/store.js
@@ -0,0 +1,72 @@
+
+const store = {
+    "102234771401894238200": {
+      items: [
+        {
+          name: "Bread",
+          info: "Buy in Lidl",
+          id: "sdfsdfsadfsdfdsf",
+          checked: false
+        },
+        {
+          name: "Cola",
+          info: "",
+          id: "gfvfsddwed",
+          checked: false
+        },
+        {
+          name: "Milk",
+          info: "Buy in Tesco",
+          id: "324rijdsojfddsaoid",
+          checked: false
+        },
+        {
+          name: "Beer",
+          info: "",
+          id: "fdswefi343fdsdf",
+          checked: false
+        },
+        {
+          name: "Bananas",
+          info: "10pcs",
+          id: "fdswefi3ddddddddddd",
+          checked: false
+        }
+      ],
+      selected: [
+        {
+          name: "Ham",
+          info: "In slices",
+          id: "43rpijdskjfna",
+          checked: false
+        },
+        {
+          name: "Rice",
+          info: "",
+          id: "e3rijfisdnc.kas3",
+          checked: false
+        },
+        {
+          name: "Potatoes",
+          info: "Buy in Tesco",
+          id: "43ifpjsdljnfew33",
+          checked: false
+        },
+        {
+          name: "Aples",
+          info: "3kg",
+          id: "ekflkdsdsaljd",
+          checked: false
+        },
+        {
+          name: "Beef",
+          info: "1kg",
+          id: "frefp43ifjdsfs",
+          checked: false
+        }
+      ],
+      costs: []
+    }
+  };
+
+  module.exports = store;
