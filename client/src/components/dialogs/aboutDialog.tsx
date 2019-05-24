ec921a790033bdfd0827bbd7c5bde5fbc808283c
Ogar <kamil.sobczyk@wp.pl>
2019-05-24 12:09:18 +0200

    add call button, style buttons

--- client/src/components/dialogs/aboutDialog.tsx
+++ client/src/components/dialogs/aboutDialog.tsx
@@ -7,19 +7,21 @@ import { Button } from '@rmwc/button';
 import { IconButton } from '@rmwc/icon-button';
 
 import { StyledDialogTitle } from './spendingsDialog/spendingsDialog';
+import { StyledButtonsContainer } from '../listBox/listsContainer';
 
 const aboutDialogButtons = [
   {
-    icon:
-      'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png',
-    size: '40px',
+    icon: 'https://image.flaticon.com/icons/svg/25/25231.svg',
     href: 'https://github.com/Ogar616/homeBudgetCombine'
   },
   {
     icon:
       'https://cdn1.iconfinder.com/data/icons/logotypes/32/square-linkedin-512.png',
-    size: '35px',
     href: 'https://www.linkedin.com/in/kamilsobczyk6/'
+  },
+  {
+    icon: 'https://freeiconshop.com/wp-content/uploads/edd/phone-flat.png',
+    href: 'tel:796224021'
   }
 ];
 
@@ -41,20 +43,23 @@ export const AboutDialog = observer(
       <DialogContent id='alert-dialog-description'>
         Coded by Kamil Sobczyk 2019
         <br />
-        {aboutDialogButtons.map(button => (
-          <IconButton
-            key={button.icon}
-            icon={button.icon}
-            style={{
-              width: button.size,
-              height: button.size,
-              backgroundSize: 'cover'
-            }}
-            tag='a'
-            target='_blank'
-            href={button.href}
-          />
-        ))}
+        <StyledButtonsContainer>
+          {aboutDialogButtons.map(button => (
+            <IconButton
+              key={button.icon}
+              icon={button.icon}
+              style={{
+                width: '35px',
+                height: '35px',
+                backgroundSize: 'cover',
+                margin: '10px'
+              }}
+              tag='a'
+              target='_blank'
+              href={button.href}
+            />
+          ))}
+        </StyledButtonsContainer>
       </DialogContent>
       <DialogActions>
         <Button onClick={() => setVisibleDialog()} color='primary' autoFocus>
