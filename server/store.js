f70ea5bb4db6ac0992e9e84b0630d36b8cd19484
Ogar <kamil.sobczyk@wp.pl>
2019-05-24 16:04:14 +0200

    add default costs

--- server/store.js
+++ server/store.js
@@ -1,72 +1,99 @@
-
 const store = {
-    "102234771401894238200": {
-      items: [
-        {
-          name: "Bread",
-          info: "Buy in Lidl",
-          id: "sdfsdfsadfsdfdsf",
-          checked: false
-        },
-        {
-          name: "Cola",
-          info: "",
-          id: "gfvfsddwed",
-          checked: false
-        },
-        {
-          name: "Milk",
-          info: "Buy in Tesco",
-          id: "324rijdsojfddsaoid",
-          checked: false
-        },
-        {
-          name: "Beer",
-          info: "",
-          id: "fdswefi343fdsdf",
-          checked: false
-        },
-        {
-          name: "Bananas",
-          info: "10pcs",
-          id: "fdswefi3ddddddddddd",
-          checked: false
-        }
-      ],
-      selected: [
-        {
-          name: "Ham",
-          info: "In slices",
-          id: "43rpijdskjfna",
-          checked: false
-        },
-        {
-          name: "Rice",
-          info: "",
-          id: "e3rijfisdnc.kas3",
-          checked: false
-        },
-        {
-          name: "Potatoes",
-          info: "Buy in Tesco",
-          id: "43ifpjsdljnfew33",
-          checked: false
-        },
-        {
-          name: "Aples",
-          info: "3kg",
-          id: "ekflkdsdsaljd",
-          checked: false
-        },
-        {
-          name: "Beef",
-          info: "1kg",
-          id: "frefp43ifjdsfs",
-          checked: false
-        }
-      ],
-      costs: []
-    }
-  };
+  "102234771401894238200": {
+    items: [
+      {
+        name: "Bread",
+        info: "Buy in Lidl",
+        id: "sdfsdfsadfsdfdsf",
+        checked: false
+      },
+      {
+        name: "Cola",
+        info: "",
+        id: "gfvfsddwed",
+        checked: false
+      },
+      {
+        name: "Milk",
+        info: "Buy in Tesco",
+        id: "324rijdsojfddsaoid",
+        checked: false
+      },
+      {
+        name: "Beer",
+        info: "",
+        id: "fdswefi343fdsdf",
+        checked: false
+      },
+      {
+        name: "Bananas",
+        info: "10pcs",
+        id: "fdswefi3ddddddddddd",
+        checked: false
+      }
+    ],
+    selected: [
+      {
+        name: "Ham",
+        info: "In slices",
+        id: "43rpijdskjfna",
+        checked: false
+      },
+      {
+        name: "Rice",
+        info: "",
+        id: "e3rijfisdnc.kas3",
+        checked: false
+      },
+      {
+        name: "Potatoes",
+        info: "Buy in Tesco",
+        id: "43ifpjsdljnfew33",
+        checked: false
+      },
+      {
+        name: "Aples",
+        info: "3kg",
+        id: "ekflkdsdsaljd",
+        checked: false
+      },
+      {
+        name: "Beef",
+        info: "1kg",
+        id: "frefp43ifjdsfs",
+        checked: false
+      }
+    ],
+    costs: [
+      { chosenItems: [], count: 1, date: "24.06.2019, 15:01" },
+      { chosenItems: ["Potatoes"], count: 3, date: "24.06.2019, 14:57" },
+      { chosenItems: ["sds"], count: 2, date: "24.06.2019, 14:57", bill: true },
+      { chosenItems: ["Aples"], count: 3, date: "24.06.2019, 14:57" },
+      { chosenItems: ["sds"], count: 2, date: "24.06.2019, 14:59", bill: true },
+      { chosenItems: ["Rice"], count: 1, date: "24.06.2019, 14:57" },
+      { chosenItems: [], count: 3, date: "24.06.2019, 14:57" },
 
-  module.exports = store;
+      { chosenItems: [], count: 1, date: "24.05.2019, 15:01" },
+      { chosenItems: ["Potatoes"], count: 3, date: "24.05.2019, 14:57" },
+      { chosenItems: ["sds"], count: 2, date: "24.05.2019, 14:57", bill: true },
+      { chosenItems: ["Aples"], count: 3, date: "24.05.2019, 14:57" },
+      { chosenItems: ["sds"], count: 2, date: "24.05.2019, 14:59", bill: true },
+      { chosenItems: ["Rice"], count: 1, date: "24.05.2019, 14:57" },
+      { chosenItems: [], count: 3, date: "24.05.2019, 14:57", bill: true },
+
+      { chosenItems: [], count: 1, date: "24.04.2019, 15:01" },
+      { chosenItems: ["Potatoes"], count: 3, date: "24.04.2019, 14:57" },
+      { chosenItems: ["sds"], count: 2, date: "24.04.2019, 14:57", bill: true },
+      { chosenItems: ["Aples"], count: 3, date: "24.04.2019, 14:57" },
+      { chosenItems: ["sds"], count: 2, date: "24.04.2019, 14:59", bill: true },
+      { chosenItems: ["Rice"], count: 1, date: "24.04.2019, 14:57" },
+      { chosenItems: [], count: 3, date: "24.04.2019, 14:57" , bill: true},
+
+
+   
+
+    ]
+  }
+};
+
+module.exports = store;
