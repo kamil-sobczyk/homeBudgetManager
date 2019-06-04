75731ece1d1797ea183d514703dbeb563d3e9e36
Ogar <kamil.sobczyk@wp.pl>
2019-06-04 15:33:32 +0200

    set headers

--- server/app.js
+++ server/app.js
@@ -2,22 +2,26 @@ require("rootpath")();
 const express = require("express");
 const bodyParser = require("body-parser");
 const routes = require("./index.js");
-// const { port } = require('./config');
+const { port } = require("./config");
 const app = express();
 const cors = require("cors");
 
-const port = 8080;
+const port = port;
 const HOST = "localhost";
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+  res.setHeader("Access-Control-Allow-Origin: *");
+  res.setHeader(
+    "Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS"
   );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 });
 
