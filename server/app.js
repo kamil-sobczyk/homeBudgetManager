fd23a79a1318977cac8379a481e0f318e6b5d3a8
Ogar <kamil.sobczyk@wp.pl>
2019-06-05 11:45:23 +0200

    simplyfy code, make cors work

--- server/app.js
+++ server/app.js
@@ -4,28 +4,12 @@ const bodyParser = require("body-parser");
 const routes = require("./index.js");
 const { port } = require("./config");
 const app = express();
-const cors = require("cors");
+const cors = require('cors');
 
-const port = port;
-const HOST = "localhost";
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin: *");
-  res.setHeader(
-    "Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 routes(app);
 
-app.listen(port, HOST);
-console.log(`Running on http://${HOST}:${port}`);
+app.listen(port);
