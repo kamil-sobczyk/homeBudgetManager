3a05eb7e446e75e8318ef730ee08b0a4a9c60c3f
Ogar <kamil.sobczyk@wp.pl>
2019-06-24 12:45:58 +0200

    remove drawer styling

--- client/src/components/mainComponents/navbar/drawer/drawer.tsx
+++ client/src/components/mainComponents/navbar/drawer/drawer.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
 
 import { Drawer, DrawerHeader, DrawerTitle, DrawerContent } from '@rmwc/drawer';
-import { List, ListItem } from '@rmwc/list';
-import styled from 'styled-components';
-import { IconButton } from '@rmwc/icon-button';
+
 import { DrawerItems } from './drawerItems';
 
 interface DrawerBarProps {
@@ -16,21 +14,14 @@ export class DrawerBar extends React.Component<DrawerBarProps, {}> {
   render() {
     const { toggleShowDrawer, showDrawer, setVisibleDialog } = this.props;
     return (
-      <>
-        <StyledDrawer modal open={showDrawer} onClose={toggleShowDrawer}>
-          <DrawerHeader>
-            <DrawerTitle>Budget Manager</DrawerTitle>
-          </DrawerHeader>
-          <DrawerContent>
-            <DrawerItems setVisibleDialog={setVisibleDialog} />
-          </DrawerContent>
-        </StyledDrawer>
-      </>
+      <Drawer modal open={showDrawer} onClose={toggleShowDrawer}>
+        <DrawerHeader>
+          <DrawerTitle>Budget Manager</DrawerTitle>
+        </DrawerHeader>
+        <DrawerContent>
+          <DrawerItems setVisibleDialog={setVisibleDialog} />
+        </DrawerContent>
+      </Drawer>
     );
   }
 }
-
-const StyledDrawer = styled(Drawer)`
-  background: #007ccc;
-  text-align: center;
-`;
