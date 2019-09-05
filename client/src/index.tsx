f180f67a320daddf403531adc38c7395ba0cce5f
Ogar <kamil.sobczyk@wp.pl>
2019-09-05 09:18:13 +0200

    adjust theme

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,42 +3,16 @@ import * as ReactDOM from 'react-dom';
 import 'normalize.css';
 
 import { ThemeProvider } from 'rmwc';
+import { themeOptions } from './lib/theme';
 import 'material-components-web/dist/material-components-web.min.css';
+
 import { App } from './components/mainComponents/App';
 
 const root = document.getElementById('root');
 
 ReactDOM.render(
-  <ThemeProvider
-    options={{
-      primary: '#303030',
-      secondary: '#661fff',
-      error: '#b00020',
-      background: '#fff',
-      surface: '#fff',
-      onPrimary: 'rgba(255, 255, 255, 1)',
-      onSecondary: 'rgba(255, 255, 255, 1)',
-      onSurface: 'rgba(0, 0, 0, 0.87)',
-      onError: '#fff',
-      textPrimaryOnBackground: 'rgba(0, 0, 0, 0.87)',
-      textSecondaryOnBackground: 'rgba(0, 0, 0, 0.54)',
-      textHintOnBackground: 'rgba(0, 0, 0, 0.38)',
-      textDisabledOnBackground: 'rgba(0, 0, 0, 0.38)',
-      textIconOnBackground: 'rgba(0, 0, 0, 0.38)',
-      textPrimaryOnLight: 'rgba(0, 0, 0, 0.87)',
-      textSecondaryOnLight: 'rgba(0, 0, 0, 0.54)',
-      textHintOnLight: 'rgba(0, 0, 0, 0.38)',
-      textDisabledOnLight: 'rgba(0, 0, 0, 0.38)',
-      textIconOnLight: 'rgba(0, 0, 0, 0.38)',
-      textPrimaryOnDark: 'white',
-      textSecondaryOnDark: 'rgba(255, 255, 255, 0.7)',
-      textHintOnDark: 'rgba(255, 255, 255, 0.5)',
-      textDisabledOnDark: 'rgba(255, 255, 255, 0.5)',
-      textIconOnDark: 'rgba(255, 255, 255, 0.5)'
-    }}
-  >
+  <ThemeProvider options={themeOptions}>
     <App />
   </ThemeProvider>,
-
   root
 );
